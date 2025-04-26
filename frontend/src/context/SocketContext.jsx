67701cab6d40bc0ccd02f73a3d20b3c674a1c8d0
manishbisht12 <154014370+manishbisht12@users.noreply.github.com>
2025-04-26 15:41:42 +0530

    url change

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -14,7 +14,7 @@ export const SocketContextProvider=({children})=>{
     const {authUser} = useAuth()
     useEffect(()=>{
         if(authUser){
-            const socket = io("https://chatters-chat-app.onrender.com/",{
+            const socket = io("https://chatters-chat-app-f6ac.onrender.com/",{
 query:{
     userId:authUser?._id,
 }
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:'http://localhost:3000',
+        target:'https://chatters-chat-app-f6ac.onrender.com/',
         secure:false,
       }
     }
